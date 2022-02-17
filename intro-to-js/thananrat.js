a705ce2694fd992025d082dc1cfa90123f2d3b00
thananrat akarapornvinit <thananrat.a@gmail.com>
2022-02-17 14:03:12 +0700

    add into-to-js page

---
+++ intro-to-js/thananrat.js
@@ -0,0 +1,12 @@
+function asMyName() {
+    const name = prompt("What is your name"); // เรียกใช้คำสั่ง prompt เพื่อถามชื่อ https://www.w3schools.com/jsref/met_win_prompt.asp
+
+    if (name != null) {
+        document.getElementById("name").innerHTML = "My name is " + name;
+    }
+  }
+
+  function helloWorld() {
+    const h1message = document.getElementById("name").innerText // ใช้คำสั่ง document.getElementById เหมือนใน askMyName เพื่อเอาข้อความใน h1 มาใส่
+       alert(h1message + ' ถึงพี่ไม่หล่อแต่ก็น่าห่อกลับบ้าน');
+    }
