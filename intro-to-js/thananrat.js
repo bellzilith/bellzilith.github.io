cc14c8927992cb68c1f47ee2835033d3b05c1c60
thananrat akarapornvinit <thananrat.a@gmail.com>
2022-02-18 14:11:59 +0700

    add calAge and loadProfile func

--- intro-to-js/thananrat.js
+++ intro-to-js/thananrat.js
@@ -6,7 +6,28 @@ function askMyName() {
     }
   }
 
-  function helloWorld() {
+function helloWorld() {
     const h1message = document.getElementById("name").innerText // ใช้คำสั่ง document.getElementById เหมือนใน askMyName เพื่อเอาข้อความใน h1 มาใส่
        alert(h1message + ' ถึงพี่ไม่หล่อแต่ก็น่าห่อกลับบ้าน');
     }
+
+function calAge() {
+    const yearOfBirth = prompt(`คุณเกิดวันปี ค.ศ. อะไร (ตัวอย่าง: 1998)`);
+    const yourAge = parseFloat(2022 - yearOfBirth);
+
+    if (yearOfBirth != null) {
+      alert(`ยินดีด้วย นายอยู่ในประเทศนี้มา ${yourAge} ปีแล้ว`);
+  }
+}
+
+function loadProfile() {
+    const name = `Thananrat`
+    const province = `Bangkok`
+    
+    if (name != null) {
+      document.getElementById("my-name").innerText = name;
+  }
+    if (province != null) {
+      document.getElementById("my-province").innerText = province;
+  }
+}
